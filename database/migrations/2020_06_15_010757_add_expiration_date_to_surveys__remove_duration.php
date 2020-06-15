8f0aff36bf3d1e0e2ace910b57a588e793728f1d
Jérémy Hoarau <292627@supinfo.com>
2020-06-15 20:16:34 +0400

    rename Class migration

--- database/migrations/2020_06_15_010757_add_expiration_date_to_surveys__remove_duration.php
+++ database/migrations/2020_06_15_010757_add_expiration_date_to_surveys__remove_duration.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddPublishDateToSurveys extends Migration
+class AddExpirationDateToSurveysRemoveDuration extends Migration
 {
     /**
      * Run the migrations.

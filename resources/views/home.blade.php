59619082950d1730db2cc977659e1e918c2eae64
Julz <you@example.com>
2020-06-25 11:24:05 +0200

    added brand images to cards and chat card container style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,13 +17,13 @@
 
 <div class="card-container">
     <div class="icon-card">
-        <img src="/images/cadena.png" alt="">
+        <img src="/images/cadena.png" alt="lock icon">
         <h3>SECURITE</h3>
         <p>Récolte de données conforme à la RGPD. Nous ne conservons, ni stockons vos questionnaires, les réponses et les analyses</p>
     </div>
 
     <div class="icon-card">
-        <img src="/images/multilangues.png" alt="">
+        <img src="/images/multilangues.png" alt="multilangues icon">
         <h3>MULTILANGUES</h3>
         <p>
             Ouverture sur le monde. Possibilité de créer des questionnaires pour vos clients à travers le monde
@@ -31,7 +31,7 @@
     </div>
 
     <div class="icon-card">
-        <img src="/images/questions.png" alt="">
+        <img src="/images/questions.png" alt="question icon">
         <h3>QUESTIONS</h3>
         <p>
             Sondages ouverts ou fermés. Définissez une durée pour vos questionnaire et ils s'acieront et se fermeront automatiquement.
@@ -39,7 +39,7 @@
     </div>
 
     <div class="icon-card">
-        <img src="/images/formats.png" alt="">
+        <img src="/images/formats.png" alt="formats icon">
         <h3>FORMATS</h3>
         <p>
             Exportation des réponses en PDF ou CSV. Afin de permettre une meilleure analyse des résultats et faciliter vos rapports.
@@ -49,17 +49,25 @@
 
 
 <div class="info-container">
-    <img src="" alt="">
-    <p>
-        Nos surveys sont sous la forme d'un chat en ligne.
-        En effet cela donna l'impression à vos clients de discuter et d'intéragir
-        avec une personne et non de répondre à une liste infinie de questions
-        comme vous pouvez retrouver sur d'autres sites de suveys.
-        De plus, vous pouvez entièrement personnaliser votre questionnaire.
-        En passant par l'image du fond, la couleur des bulles, l'avatar de votre client,
-        le format et la taille des bulles, et bien plus encore.
-        Vous pouvez cliquez sour le bouton ci-dessous pour tester un de nos questionnaires.
-    </p>
+    <div class="item">
+        <img src="" alt="">
+  </div>
+
+  <div class="item2">
+      <h1>NOS</h1>
+      <p>- Chattez avec Biro-</p>
+        <p>
+            Nos surveys sont sous la forme d'un chat en ligne.
+            En effet cela donna l'impression à vos clients de discuter et d'intéragir
+            avec une personne et non de répondre à une liste infinie de questions
+            comme vous pouvez retrouver sur d'autres sites de suveys.
+            De plus, vous pouvez entièrement personnaliser votre questionnaire.
+            En passant par l'image du fond, la couleur des bulles, l'avatar de votre client,
+            le format et la taille des bulles, et bien plus encore.
+            Vous pouvez cliquez sour le bouton ci-dessous pour tester un de nos questionnaires.
+        </p>
+        <a href="#">En savoir plus</a>
+</div>
 
 </div>
 
@@ -70,21 +78,21 @@
 
 <div class="card-container">
     <div class="cards">
-        <img src="/images/adobe.png" alt="">
+        <img src="/images/adobe.svg" alt="adobe logo">
         <p>
             "Tout ce que l'on peut imaginer, on peut le créer avec Big Brother"
         </p>
     </div>
 
     <div class="cards">
-        <img src="" alt="">
+        <img src="/images/carrefour.svg" alt="carrefour logo">
         <p>
             "Simple et facile d'utilisation. Un plaisir pour les yeux avec sa colletion de design complet."
         </p>
     </div>
 
     <div class="cards">
-        <img src="" alt="">
+        <img src="/images/microsoft.svg" alt="microsoft logo">
         <p>
             "Big Brother est un outil intuitif et performant qui nous permet de nous développer et nous perfectionner"
         </p>
